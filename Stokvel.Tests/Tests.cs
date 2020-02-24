3741de8628d7f9fd7421c4823b51464f8b49bd75
Yusuf Motara <cinyc.s@gmail.com>
2020-02-24 17:22:17 +0200

    Use mocks instead of concrete objects

--- Stokvel.Tests/Tests.cs
+++ Stokvel.Tests/Tests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using StokvelNamespace;
+using NSubstitute;
 
 namespace StokvelNamespace.Tests
 {
@@ -20,18 +21,21 @@ namespace StokvelNamespace.Tests
         public void CheckThatPayoutsOccurCorrectly() {
             // ARRANGE
             IStokvel s = new Stokvel(4, 1000);
-            IMember a = new Member("Adam", s);
-            IMember b = new Member("Bob", s);
-            IMember c = new Member("Chloe", s);
+            IMember a = Substitute.For<IMember>();
+            s.AddMember(a);
+            IMember b = Substitute.For<IMember>();
+            s.AddMember(b);
+            IMember c = Substitute.For<IMember>();
+            s.AddMember(c);
             // ACT
             s.NextMeeting();
             s.NextMeeting();
             s.NextMeeting();
             s.NextMeeting();
             // ASSERT
-            Assert.That(a.Withdrawn, Is.EqualTo(9000));
-            Assert.That(b.Withdrawn, Is.EqualTo(0));
-            Assert.That(c.Withdrawn, Is.EqualTo(0));
+            a.Received(1).PayOut(Arg.Any<int>());
+            b.DidNotReceive().PayOut(Arg.Any<int>());
+            c.DidNotReceive().PayOut(Arg.Any<int>());
         }
 
         [Test]
